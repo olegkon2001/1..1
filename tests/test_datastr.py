de3685a7546952d6747b0fb7b9dcb650bc213ac2
000 <oleg_konovalov_2001@mail.ru>
2023-02-21 11:12:51 +0300

    1

--- tests/test_datastr.py
+++ tests/test_datastr.py
@@ -1,22 +1,34 @@
-import pytest
+import unittest
 from scr.datastr import Node,Stack
 
-@pytest.fixture
-def node_10():
-    return Node(10)
-
-
-
-def test_node_init(node_10):
-    assert node_10.data == 10
-    assert node_10.next_node is None
-
-def test_node_next_node(node_10):
-    """Проверим, что next_node ссылается на узел."""
-    node2 = Node(220, node_10)
-    assert node2.next_node is node_10
 
 
 
 
+class TestNode(unittest.TestCase):
+    """Тест класса Node"""
 
+    def setUp(self) -> None:
+        self.node_10 = Node(10)
+
+
+
+    def test_node_init1(self):
+        assert self.node_10.data == 10
+        self.node_10 = Node(100)
+        assert self.node_10.next_node is None
+
+    def test_node_init2(self):
+        assert self.node_10.data == 10
+
+
+
+    def test_node_next_node(self):
+        """Проверим, что next_node ссылается на узел."""
+        node2 = Node(220, self.node_10)
+        assert node2. next_node is self.node_10
+
+
+class TestStack(unittest.TestCase):
+    def test_stack_top(self):
+        pass
